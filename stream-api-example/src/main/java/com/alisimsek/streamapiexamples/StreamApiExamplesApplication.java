c563d71ee96bc287b2fcfc71794f8d4346307393
Ali <ali.simsek035@gmail.com>
2025-04-10 23:34:28 +0300

    Initial commit

---
+++ stream-api-example/src/main/java/com/alisimsek/streamapiexamples/StreamApiExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.alisimsek.streamapiexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApiExamplesApplication.class, args);
+		System.out.println("stream api examples application started");
+	}
+
+}
---
+++ stream-api-example/src/test/java/com/alisimsek/streamapiexamples/StreamApiExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alisimsek.streamapiexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
