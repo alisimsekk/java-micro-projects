3ec04b96b6c4c3440ba7e199e038c34b0d25d2e3
Ali <ali.simsek035@gmail.com>
2025-04-25 23:46:17 +0300

    Initial commit QueryDSL project

---
+++ querydsl-examples/src/main/java/com/alisimsek/querydslexamples/QuerydslExamplesApplication.java
@@ -0,0 +1,18 @@
+package com.alisimsek.querydslexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for QueryDSL Examples project
+ * 
+ * This application demonstrates QueryDSL functionality.
+ */
+@SpringBootApplication
+public class QuerydslExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslExamplesApplication.class, args);
+	}
+
+}
