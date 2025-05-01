1356113a7b29cbe94ec83a8002097926f5c7fa22
Ali <ali.simsek035@gmail.com>
2025-05-01 23:30:14 +0300

    Added categoryDto

---
+++ querydsl-examples/src/main/java/com/alisimsek/querydslexamples/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.alisimsek.querydslexamples.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    private Long id;
+    private String name;
+    private String description;
+} 
