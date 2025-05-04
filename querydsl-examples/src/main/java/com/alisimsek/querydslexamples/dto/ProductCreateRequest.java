0da849719910e1a5edc15d42e1298b551518c5ed
Ali <ali.simsek035@gmail.com>
2025-05-04 23:19:23 +0300

    Added productCreateRequest

---
+++ querydsl-examples/src/main/java/com/alisimsek/querydslexamples/dto/ProductCreateRequest.java
@@ -0,0 +1,21 @@
+package com.alisimsek.querydslexamples.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreateRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+    private Long categoryId;
+} 
